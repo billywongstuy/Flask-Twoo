633c55224b80763a5cae94de196974bb03e9e97d
Billy Wong <bwong5@stuy.edu>
2016-09-22 21:17:37 -0400

    first commit at home... should've commited in between

--- app.py
+++ app.py
@@ -1,47 +1,47 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
 import csv
 import random
 
-sheet = csv.reader(open("occupations.csv"))
-sheet.next()
-jobs = {}
+def chooseJob():
+    sheet = csv.reader(open("occupations.csv"))
+    jobs = {}
 
 
-##GET OCCUPATIONS.CSV
-
-for row in sheet:
-    if row[0] != "Total":
-        job = row[0]
-        percent = float(row[1])
-        jobs[job] = percent
-
-#print jobs 
-
-tracker = float(random.randrange(998))/10
-
-print "Random: " + str(rand)
+    for row in sheet:
+        if row[0] != "Total" and row[1] != "Percentage":
+            job = row[0]
+            percent = float(row[1])
+            jobs[job] = percent
 
 
-#This is kind of unefficient because the loop doesn't
-#stop until the last value no matter what
-for key in jobs:
-    if tracker != 20000:
-        if jobs[key] >= tracker:
-            print key 
-            tracker = 20000
-        else:
-            tracker -= jobs[key]
+    tracker = float(random.randrange(998))/10
 
 
-def beginning(String title):
+    for key in jobs:
+        if tracker != 20000:
+            if jobs[key] >= tracker:
+                tracker = 20000
+                return key          
+            else:
+                tracker -= jobs[key]
+
+
+def beginning(title):
     return '''
     <!DOCTYPE html>
     <html>
-    <head>''' + title + '''</head>
-    <body>
-    
+    <head>''' + title + '''
+
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css'>
+    <link type="text/css" rel="stylesheet" href="static/index-stylesheet.css">
+
+    <style>
+    body {font-family: Ubuntu}
+    </style>
+
+    </head>
+    <body>    
     '''
 
 def end():
@@ -50,25 +50,33 @@ def end():
     </html>
     '''
 
+def makeTable():
+    store = "<table border=\"1px\">"
+    sheet = csv.reader(open("occupations.csv"))
+    for row in sheet:
+        store += "<tr>"
+        store += "<td>" + row[0] + "</td><td>" + row[1] + "</td>" 
+        store += "</tr>"
+    store += "</table></br>"
+    return store
+
+
 @app.route("/")
 def helloworld():
     return render_template("index.html")
 
 @app.route("/occupations")
 def occupy_wallstreet():
-    return 
+    text = beginning("Occupations") + "<div id=\"header\" style=\"font-family:'Ubuntu';\">" + makeTable() + "</br>" + chooseJob()+ "</div>" + end() 
+    return text
 
-@app.route("/notsecret")
-def something():
-    return "you cannot read this"
-
-
-coll = [1,2,3,4,5]
-
-@app.route("/first_temp")
+@app.route("/fun")
 def test_temp():
-    return "none"
-    #return render_template("main.html",foo="SILENCIO",thing=coll)
+    stuff = '''
+    This is an almost blank page. </br> 
+    Oh, and Jinja2, if you put "|safe" after a variable, html tags work!
+    '''
+    return render_template("template.html",Title="Fun",body=stuff)
 
 
 if __name__ == "__main__":
